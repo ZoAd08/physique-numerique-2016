6e15369032f09c40267e2cd52b61fde1ed1f73e0
gymthana <djinthana@gmail.com>
2016-12-06 10:39:39 +0100

    gymth-densite

--- Modele_2/densiteradiale.py
+++ Modele_2/densiteradiale.py
@@ -23,33 +23,36 @@ sens = "Comparaison entre " + str(config[2,0]) + " seconde et " + str(config[2,1
 pas=radius / 100.
 
 data = np.loadtxt("marcheur.txt")
-x=np.zeros((2,marcheur))
-y=np.zeros((2,marcheur))
+x=np.zeros(marcheur)
+y=np.zeros(marcheur)
 points=np.zeros((marcheur,2))
+densite=np.zeros((len(data[:,0])/marcheur)+2,radius/pas)
 
+for j in range(1,(len(data[:,0])/marcheur)-1)):
 
-x[0,:]=data[0:marcheur,0].transpose()
-y[0,:]=data[0:marcheur,1].transpose()
-x[1,:]=data[len(data[:,0])-marcheur:len(data[:,0]),0].transpose()
-y[1,:]=data[len(data[:,0])-marcheur:len(data[:,0]),1].transpose()
+    x=data[marcheur*j:(marcheur-1)*(j+1)),0].transpose()
+    y=data[marcheur*j:(marcheur-1)*(j+1),1].transpose()
 
-points=zip(x[1,:],y[1,:])
-nb_points = []
-densite = []
-R = []
-T=KDTree(points)
+    points=zip(x,y)
+    nb_points = []
+    R = []
+    T=KDTree(points)
 
-for i in np.linspace(0,radius-pas,radius/pas):
-    nb_points.append(len(T.query_ball_point([0,0],r= i + pas)) - len(T.query_ball_point([0,0],r= i)))
-    densite.append((len(T.query_ball_point([0,0],r= i + pas)) - len(T.query_ball_point([0,0],r= i)))/(np.pi*(2*i*pas+pas**2)))
-    R.append((i + 0.5*pas))
+    for i in np.linspace(0,radius-pas,radius/pas):
+        densite[j,i]=((len(T.query_ball_point([0,0],r= i + pas)) - len(T.query_ball_point([0,0],r= i)))/(np.pi*(2*i*pas+pas**2)))
+        R.append((i + 0.5*pas))
 
-#densite=zip(R,nb_points)
-# print densite
+for l in np.linspace(0,radius-pas,radius/pas):
+        R.append((l + 0.5*pas))
+
+for k in range(radius/pas):
+    densite[-2,k]=mean(densite[0:-3,k])
+    densite[-1,k]=std(densite[0:-3,k])
+
+moyenne
 
 plt.figure(4)
-#p1=plt.plot(R,nb_points, 'b-.o', label="Nombre de bacteries", linewidth=2.0)
-p2=plt.plot(R,densite, 'r-.o',label="Densite surfacique de bacteries", linewidth=2.0)
+p2=plt.plot(R,densite[-2,:], 'r-.o',label="Densite surfacique de bacteries", linewidth=2.0)
 plt.xlabel("Rayon")
 plt.legend()
 plt.show()
